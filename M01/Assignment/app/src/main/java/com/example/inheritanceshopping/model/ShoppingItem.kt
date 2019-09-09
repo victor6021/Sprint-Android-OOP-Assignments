1409d309af239099b4eb05a5586a0591f966f858
victor6021 <victor60216021@gmail.com>
2019-09-09 12:37:09 -0700

    added class - getdisplayname

--- M01/Assignment/app/src/main/java/com/example/inheritanceshopping/model/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/example/inheritanceshopping/model/ShoppingItem.kt
@@ -1,5 +1,9 @@
 package com.example.inheritanceshopping.model
 
-class ShoppingItem(val colorId: Int, val productName: String)
+open class ShoppingItem(val colorId: Int, val productName: String)
 
-class getDisplayName(colorId: Int, productName: String)
+class getDisplayName(colorId: Int, productName: String): ShoppingItem(colorId, productName){
+    open fun returnProductName():String{
+        return "$productName"
+    }
+}
