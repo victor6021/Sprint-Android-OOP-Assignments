2cc704ab96de42a4f521128c1e25d7976759a819
victor6021 <victor60216021@gmail.com>
2019-09-09 13:01:29 -0700

    undo what i did,added the getDisplayName method

--- M01/Assignment/app/src/main/java/com/example/inheritanceshopping/model/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/example/inheritanceshopping/model/ShoppingItem.kt
@@ -1,9 +1,19 @@
 package com.example.inheritanceshopping.model
 
-open class ShoppingItem(val colorId: Int, val productName: String)
-
-class getDisplayName(colorId: Int, productName: String): ShoppingItem(colorId, productName){
-    open fun returnProductName():String{
-        return "$productName"
+open class ShoppingItem(val colorId: Int, val productName: String){
+    val returnDisplayName = "$productName"
+    open fun getDisplayName(): String{
+        return "$returnDisplayName"
     }
+}
+
+class food(colorId: Int, productName: String):ShoppingItem(colorId, productName){
+    val colorId:Int = $(".color")
+
+class clothing(colorId: Int, productName: String):ShoppingItem(colorId, productName){
+
+}
+
+class electronic(colorId: Int, productName: String):ShoppingItem(colorId, productName){
+
 }
